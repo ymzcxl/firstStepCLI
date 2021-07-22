1836ef697dadd3bb75f8ffb9178e58536058d4f0
yangmingzhao <2535875136@qq.com>
2021-07-22 15:25:34 +0800

    fix: 测试提交成功不

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import ElementPlus from 'element-plus'
 import App from './App.vue'
 import router from './router/index'
 import store from './store/index'
-
 import 'element-plus/lib/theme-chalk/index.css'
 
 createApp(App).use(router).use(store).use(ElementPlus).mount('#app')
