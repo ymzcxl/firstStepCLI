6c5bb463e62050c0db4632001f754216829ea7ea
yangmingzhao <2535875136@qq.com>
2022-03-05 15:03:25 +0800

    初始化项目

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { createApp } from "vue";
 import ElementPlus from "element-plus";
-import App from "./App.vue";
-import router from "./router/index";
-import store from "./store/index";
+import App from "@/App.vue";
+import router from "@/router/index";
+import store from "@/store/index";
 import "element-plus/lib/theme-chalk/index.css";
-import swiper from "./plugins/swiper";
+import swiper from "@/plugins/swiper";
 
 createApp(App).use(router).use(store).use(ElementPlus)
   .use(swiper)
